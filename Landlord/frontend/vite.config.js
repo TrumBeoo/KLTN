5b3113403f222213c502e501da040603608b1ff8
trung30302 <txtrung30302@gmail.com>
2026-06-15 23:01:04 +0700

    update Landlord index

--- Landlord/frontend/vite.config.js
+++ Landlord/frontend/vite.config.js
@@ -26,15 +26,12 @@ export default defineConfig({
     },
     chunkSizeWarningLimit: 1000,
     sourcemap: false,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
-      }
-    }
+    minify: 'esbuild'
   },
   optimizeDeps: {
     include: ['react', 'react-dom', 'react-router-dom', '@mui/material', '@mui/icons-material']
+  },
+  esbuild: {
+    drop: ['console', 'debugger']
   }
 })
